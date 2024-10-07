c5ae39cc8815c15e1643c8ae0d2b7b6025f9dbe1
Zelskieee <chojjaarif@gmail.com>
2024-10-07 15:56:48 +0800

    let's begin

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body{
+            display:flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center ;
+            background-image: linear-gradient(to right, rgba(255,0,0,0), rgba(255,0,0,1));
+                }
+    </style>
+</head>
+<body>
+    <div style="text-align: center;">
+        <h1>My Latest Biodata</h1>
+    </div>
+    <table style="border-collapse: collapse; width: 50%; border-radius: 10px; overflow: hidden;">
+    <tr>
+        <th width="10%" style="border: 1px solid black; padding: 10px; background-color: grey; color: white;">Name</th>
+        <td style="border: 1px solid black; padding: 10px;">Arif Azinuddin</td>
+        <td colspan="2" style="border: 1px solid black; padding: 10px; text-align: center;">
+        <img src='img/arifblazer.png' width="100" height="100">
+        </td>
+    </tr>
+    <tr>
+      <th style="border: 1px solid black; padding: 10px; background-color: grey; color: white;">Date Of Birth</th>
+      <td colspan="2" style="border: 1px solid black; padding: 10px;">20 February 2002</td>
+    </tr>
+    <tr>
+      <th style="border: 1px solid black; padding: 10px; background-color: grey; color: white;">Religion</th>
+      <td colspan="2" style="border: 1px solid black; padding: 10px;">Islam</td>
+    </tr>
+    <tr>
+      <th style="border: 1px solid black; padding: 10px; background-color: grey; color: white;">Race</th>
+      <td colspan="2" style="border: 1px solid black; padding: 10px;">Malay</td>
+    </tr>
+    <tr>
+      <th rowspan="4" style="border: 1px solid black; padding: 10px; background-color: grey; color: white;">Address</th>
+      <th style="border: 1px solid black; padding: 10px; background-color: grey; color: white;">Street</th>
+      <td style="border: 1px solid black; padding: 10px;">1700 Jalan Anggerik 1/10</td>
+    </tr>
+    <tr>
+      <th style="border: 1px solid black; padding: 10px; background-color: grey; color: white;">Town</th>
+      <td style="border: 1px solid black; padding: 10px;">Bandar Tenggara</td>
+    </tr>
+    <tr>
+      <th style="border: 1px solid black; padding: 10px; background-color: grey; color: white;">District</th>
+      <td style="border: 1px solid black; padding: 10px;">Kulai</td>
+    </tr>
+    <tr>
+      <th style="border: 1px solid black; padding: 10px; background-color: grey; color: white;">State</th>
+      <td style="border: 1px solid black; padding: 10px;">Johor</td>
+    </tr>
+    <tr>
+      <th rowspan="3" style="border: 1px solid black; padding: 10px; background-color: grey; color: white;">Contact</th>
+      <th style="border: 1px solid black; padding: 10px; background-color: grey; color: white;">Mobile Phone</th>
+      <td style="border: 1px solid black; padding: 10px;">011-1079 4886</td>
+    </tr>
+    <tr>
+      <th style="border: 1px solid black; padding: 10px; background-color: grey; color: white;">Social Media</th>
+      <td style="border: 1px solid black; padding: 10px;">LinkedIn : <a href="https://www.linkedin.com/in/arif-azinuddin-a3788b22b/" target="_blank">Arif Azinuddin</a></td>
+    </tr>
+    <tr>
+      <th style="border: 1px solid black; padding: 10px; background-color: grey; color: white;">Website</th>
+      <td style="border: 1px solid black; padding: 10px;">YouTube : <a href="https://www.youtube.com/@zelskieee" target="_blank">Arif Azinuddin</a></td>
+    </tr>
+    <tr>
+      <th style="border: 1px solid black; padding: 10px; background-color: grey; color: white;">Course and Institution</th>
+      <td colspan="2" style="border: 1px solid black; padding: 10px;">Software Engineering at Tun Hussein Onn University of Malaysia</td>
+    </tr>
+    <tr>
+      <th style="border: 1px solid black; padding: 10px; background-color: grey; color: white;">Programming Skills</th>
+      <td colspan="2" style="border: 1px solid black; padding: 10px;">C, C++, C#, Java, and Python</td>
+    </tr>
+    <tr>
+      <th style="border: 1px solid black; padding: 10px; background-color: grey; color: white;">Personal Interest</th>
+      <td colspan="2" style="border: 1px solid black; padding: 10px;">Gym and Sport</td>
+    </tr>
+    <tr>
+      <th style="border: 1px solid black; padding: 10px; background-color: grey; color: white;">Future Plan in 2024</th>
+      <td colspan="2" style="border: 1px solid black; padding: 10px;">Working as a Software Tester at MSTB</td>
+    </tr>
+  </table>
+</body>
+</html>
